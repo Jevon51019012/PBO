b5642b397be1d75af9064dee02536776c4fa654f
Jevon51019012 <91229814+Jevon51019012@users.noreply.github.com>
2021-09-30 11:42:41 +0800

    Add files via upload

---
+++ praktikum PBO/pertemuan ke-2/percobaan 5.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class prak3{
+    public static void main(String args[]) {
+      string a1="12";
+      string a2="22";
+      
+      int nilai1 = Integer.parseInt (a1);
+      int nilai2 = Integer.parseInt (a2);
+      int hasil = nilai1 + nilai2;
+    
+      System.out.println("hasil " +nilai1+ "+" +nilai2+ "=" +hasil);
+    }
+}
